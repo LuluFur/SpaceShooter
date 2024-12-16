3700be72a9a439158fcfd81b999b0cdaf3b4055e
Lulu <lwhitham05@gmail.com>
2024-12-16 01:45:50 +0000

    Update AlienRammer.js

--- src/entities/AlienRammer.js
+++ src/entities/AlienRammer.js
@@ -16,20 +16,12 @@ class AlienRammer extends Alien {
     push();
     translate(this.position.x, this.position.y);
     rotate(radians(this.direction) + HALF_PI);
-
-    if (this.isVisible || this.isFlashing) {
-      // Normal appearance or flashing
-      stroke(100, 100, 100, 255); // Solid color when visible
-      strokeWeight(2);
-      noFill();
-      triangle(0, -this.size, -this.size / 2, this.size, this.size / 2, this.size);
-    } else {
-      // Stealth appearance
-      stroke(255, 80, 80); // High transparency
-      strokeWeight(1);
-      noFill();
-      triangle(0, -this.size, -this.size / 2, this.size, this.size / 2, this.size);
-    }
+    
+    // Stealth appearance
+    stroke(255, 80, 80); // High transparency
+    strokeWeight(1);
+    noFill();
+    triangle(0, -this.size, -this.size / 2, this.size, this.size / 2, this.size);
 
     pop();
   }
