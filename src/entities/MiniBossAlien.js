f3a26474060848bf2b1724f736063be5817df90f
Lulu <lwhitham05@gmail.com>
2024-12-16 00:53:05 +0000

    Update MiniBossAlien.js

--- src/entities/MiniBossAlien.js
+++ src/entities/MiniBossAlien.js
@@ -7,7 +7,7 @@
 class MiniBossAlien extends Alien {
   constructor(x, y) {
     super(x, y);
-    const p5 = getP5();
+    
     
     // Double the size
     this.size = 40;
@@ -34,26 +34,26 @@ class MiniBossAlien extends Alien {
   }
 
   shoot() {
-    const p5 = getP5();
+    
     if (!entities.player) return;
 
-    const angleToPlayer = p5.atan2(
+    const angleToPlayer = atan2(
       entities.player.position.y - this.position.y,
       entities.player.position.x - this.position.x
     );
 
     // Less inaccuracy than regular aliens
     const inaccuracy = (1 - this.intelligenceLevel) * 0.25;
-    const finalAngle = angleToPlayer + p5.random(-inaccuracy, inaccuracy);
+    const finalAngle = angleToPlayer + random(-inaccuracy, inaccuracy);
 
-    const velocity = p5.createVector(
-      p5.cos(finalAngle),
-      p5.sin(finalAngle)
+    const velocity = createVector(
+      cos(finalAngle),
+      sin(finalAngle)
     ).mult(3); // Slightly slower projectiles
 
     const projectile = new Projectile(
-      this.position.x + p5.cos(finalAngle) * 30,
-      this.position.y + p5.sin(finalAngle) * 30,
+      this.position.x + cos(finalAngle) * 30,
+      this.position.y + sin(finalAngle) * 30,
       velocity,
       this
     );
@@ -61,21 +61,21 @@ class MiniBossAlien extends Alien {
     projectile.damage = this.damage;
     projectile.size = this.projectileSize;
     this.projectiles.push(projectile);
-    this.lastShotTime = p5.millis();
+    this.lastShotTime = millis();
     playSound('shootSound');
   }
 
   draw() {
-    const p5 = getP5();
+    
     
     // Draw projectiles
     for (let proj of this.projectiles) {
       proj.draw();
     }
     
-    p5.push();
-    p5.translate(this.position.x, this.position.y);
-    p5.rotate(p5.radians(this.direction) + p5.HALF_PI);
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(radians(this.direction) + HALF_PI);
 
     // Pulsing glow effect
     this.glowIntensity += 0.05 * this.glowDirection;
@@ -83,51 +83,51 @@ class MiniBossAlien extends Alien {
     if (this.glowIntensity <= 0) this.glowDirection = 1;
 
     // Enhanced glow effect
-    p5.drawingContext.shadowBlur = 20 + (this.glowIntensity * 10);
-    p5.drawingContext.shadowColor = 'rgba(255, 0, 0, 0.5)';
+    drawingContext.shadowBlur = 20 + (this.glowIntensity * 10);
+    drawingContext.shadowColor = 'rgba(255, 0, 0, 0.5)';
 
     // Draw boss alien ship
-    p5.stroke(255, 0, 0);
-    p5.strokeWeight(3);
-    p5.noFill();
+    stroke(255, 0, 0);
+    strokeWeight(3);
+    noFill();
     
     // More complex ship design
-    p5.beginShape();
-    p5.vertex(0, -this.size);
-    p5.vertex(-this.size/2, 0);
-    p5.vertex(-this.size/3, this.size/2);
-    p5.vertex(0, this.size/3);
-    p5.vertex(this.size/3, this.size/2);
-    p5.vertex(this.size/2, 0);
-    p5.endShape(p5.CLOSE);
+    beginShape();
+    vertex(0, -this.size);
+    vertex(-this.size/2, 0);
+    vertex(-this.size/3, this.size/2);
+    vertex(0, this.size/3);
+    vertex(this.size/3, this.size/2);
+    vertex(this.size/2, 0);
+    endShape(CLOSE);
 
     // Additional details
-    p5.line(-this.size/4, -this.size/2, this.size/4, -this.size/2);
-    p5.line(-this.size/3, this.size/4, this.size/3, this.size/4);
+    line(-this.size/4, -this.size/2, this.size/4, -this.size/2);
+    line(-this.size/3, this.size/4, this.size/3, this.size/4);
 
-    p5.drawingContext.shadowBlur = 0;
-    p5.pop();
+    drawingContext.shadowBlur = 0;
+    pop();
 
     // Draw health bar above boss
     this.drawHealthBar();
   }
 
   drawHealthBar() {
-    const p5 = getP5();
+    
     const barWidth = this.size * 2;
     const barHeight = 5;
     const x = this.position.x - barWidth/2;
     const y = this.position.y - this.size - 15;
 
-    p5.push();
-    p5.noStroke();
+    push();
+    noStroke();
     // Background
-    p5.fill(100, 0, 0);
-    p5.rect(x, y, barWidth, barHeight);
+    fill(100, 0, 0);
+    rect(x, y, barWidth, barHeight);
     // Health
     const healthWidth = (this.health / this.maxHealth) * barWidth;
-    p5.fill(255, 0, 0);
-    p5.rect(x, y, healthWidth, barHeight);
-    p5.pop();
+    fill(255, 0, 0);
+    rect(x, y, healthWidth, barHeight);
+    pop();
   }
 }
